51186a56471263cf93e5d51246372360c286fb61
Nandasejudith <judithnandase@gmail.com>
2021-05-06 19:25:48 +0700

    cut kotlin

---
+++ src/kotlin.kt
@@ -0,0 +1,32 @@
+fun main(){
+introduction("judith",20)
+    serve()
+}
+
+fun introduction(name: String, age: Int){
+    var linya="judith"
+    var myaka=20
+    var statement=("my name is $linya and I am $myaka years old")
+    println(statement)
+}
+fun serve(){
+    var age=15
+    if (age<=6){
+        println("glass of milk")
+    }
+    else if (age>=14){
+        println("searve fanta")
+    }
+    else if(age<=15){
+        println("searve cocacola")
+    }
+    else{
+        println("qge")
+    }
+}
+
+fun names(){
+    var names=arrayOf("Nandase","Kutesa","Mary","Agnes")
+    for (name in names)
+        if (name%)
+}
